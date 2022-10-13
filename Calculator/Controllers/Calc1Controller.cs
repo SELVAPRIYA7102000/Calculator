3c0a4d3b131af7449b111fec2d1b59494bfa5e1f
STS663-SELVAPRIYA R <STS663-SELVAPRIYA R@STS663L-SELVAPR>
2022-10-13 14:45:27 +0530

    Add project files.

---
+++ Calculator/Controllers/Calc1Controller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Calculator.Models;
+namespace Calculator.Controllers
+{
+    public class Calc1Controller : Controller
+    {
+        // GET: Calc1
+        public ActionResult Index()
+        {
+            return View(new Calc());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Calc c)
+        {
+            c.Result = 0;
+            switch(c.Op)
+            {
+               
+                case '+':
+                    c.Result = c.num1 + c.num2;
+                    break;
+                case '-':
+                    c.Result = c.num1 - c.num2;
+                    break;
+                case '*':
+                    c.Result = c.num1 * c.num2;
+                    break;
+                case '/':
+                    c.Result = c.num1 / c.num2;
+                    break;
+                case 'C':
+                    c.Result = 0;
+                    break;
+            }
+            c.num2 = c.Result;
+            c.num1 = 0;
+            return View(c);
+        }
+    }
+}
---
+++ Calculator/Models/Calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator.Models
+{
+    public class Calc
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int Result { get; set; }
+        public char Op { get; set; }
+        
+    }
+}
